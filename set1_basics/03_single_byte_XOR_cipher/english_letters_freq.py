e4aa8180d9277cbaf130638e007d0b64296476c0
VintherWolf <danielvinther@youmail.dk>
2021-03-22 00:53:35 +0100

    Add set1_03 single byte xor

---
+++ set1_basics/03_single_byte_XOR_cipher/english_letters_freq.py
@@ -0,0 +1,8 @@
+# source: https://www3.nd.edu/~busiforc/handouts/cryptography/letterfrequencies.html
+
+
+MOST_FREQUENT_LETTERS = {
+    'e':    0.11,   'a':    0.085, 'r':  0.075,
+    'i':    0.075,  'o':    0.072,  't':    0.069,
+    'n':    0.066,  's':    0.057,  'l':    0.055
+}
